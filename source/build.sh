d63b156554835a12ccf368f96cbebe892abfca6c
mallo321123 <franz.huber1@gmx.de>
2026-06-17 10:16:18 +0200

    Fix Build process

--- source/build.sh
+++ source/build.sh
@@ -5,9 +5,9 @@ TRANSLATION_DIR="../Translations"
 
 # AVAILABLE_LANGUAGES will be calculating according to json files in $TRANSLATION_DIR
 AVAILABLE_LANGUAGES=("DE" "EN" "ES" "FR" "IT" "NL" "PL" "PT" "RU" "TR" "ZH")
-BUILD_LANGUAGES=()
+BUILD_LANGUAGES=("DE")
 AVAILABLE_MODELS=("TS100" "TS80" "TS80P" "Pinecil" "MHP30" "Pinecilv2" "S60" "S60P" "T55" "TS101")
-BUILD_MODELS=()
+BUILD_MODELS=("Pinecilv2" "Pinecil")
 OPTIONS=()
 
 builder_info() {
